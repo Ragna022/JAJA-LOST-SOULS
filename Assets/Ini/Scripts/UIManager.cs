6ff4fb3dc0fd25bb55c0dbcbafb3abd59dd61e54
Iniubong Daniel Offiong <iniubongdaniel5@gmail.com>
2025-11-01 00:57:36 +0100

    Lobby UI Implementation
    - Implemented Lobby UI
    - Updated the UI flow to the current flow on the Figma file

--- Assets/Ini/Scripts/UIManager.cs
+++ Assets/Ini/Scripts/UIManager.cs
@@ -11,11 +11,10 @@ public class UIManager : MonoBehaviour
 {
     [Header("UI Screens")]
     [SerializeField] private RectTransform[] UIScreens;
-    [SerializeField] private GameObject mainMenuButtonHolder;
     [SerializeField] private float fadeDuration = 1f;
 
     [Header("Canvas Groups")]
-    [SerializeField] private CanvasGroup splashUICG, loadingUICG, menuUICG, mainMenuUICG, charSelectionUICG, settingsUICG;
+    [SerializeField] private CanvasGroup splashUICG, loadingUICG, menuUICG, charSelectionUICG, gameModeUICG, hostingUI, gameIDUI, lobbyUI;
 
     [Header("Loading UI")]
     [SerializeField] private Slider progressBar;
@@ -26,21 +25,33 @@ public class UIManager : MonoBehaviour
     [SerializeField] private AudioSource sfxAS;
     [SerializeField] private AudioClip buttonClick;
 
+    public GameObject[] playerImageBg;
+
+    private CameraAnimator camAnim;
+
+    public TextMeshProUGUI readyText;
+    public CharacterSelector charView;
+
     [Header("Audio Settings")]
     [SerializeField] private Slider musicSlider, sfxSlider;
 
-    private bool isMenu, isMainMenu;
-
-    private ColorAdjustments colorAdjustments;
+    private bool isMenu;
 
     private void Start()
     {
+        if (camAnim == null)
+        {
+            camAnim = FindFirstObjectByType<CameraAnimator>(FindObjectsInactive.Include);
+
+            if (camAnim == null)
+                Debug.LogError("UIManager: CharacterSelector (camAnim) not found in scene!");
+        }
+
         StartCoroutine(CloseSplashThenLoadMain());
 
         // Load saved prefs (defaults if not set)
         float savedMusicVol = PlayerPrefs.GetFloat("MusicVol", 0.75f);
         float savedSFXVol = PlayerPrefs.GetFloat("SFXVol", 0.75f);
-        float savedBrightness = PlayerPrefs.GetFloat("Brightness", 1f);
 
         // Apply to sliders
         musicSlider.value = savedMusicVol;
@@ -60,48 +71,23 @@ public class UIManager : MonoBehaviour
         yield return new WaitForSeconds(Random.Range(4.5f, 8f));
 
         splashUICG.DOFade(0, fadeDuration);
+        SetCanvasGroupInActive(splashUICG);
         yield return new WaitForSeconds(fadeDuration);
 
         yield return StartCoroutine(ShowLoadingUI(menuUICG));
     }
 
-    public void ClosemenuUICGThenLoadSettings()
-    {
-        StartCoroutine(MenuToSettings());
-    }
-
-    private IEnumerator MenuToSettings()
+    public void MenuToSettings()
     {
         isMenu = true;
-            yield return menuUICG.DOFade(0, fadeDuration);
+            SetCanvasGroupInActive(menuUICG);
                 ShowSettingsUI();
     }
-
-    public void CloseMainMenuUICGThenLoadSettings()
+    
+    public void BackToMenuUI()
     {
-        StartCoroutine(MainMenuToSettings());
-    }
-
-    private IEnumerator MainMenuToSettings()
-    {
-        isMainMenu = true;
-            yield return mainMenuUICG.DOFade(0, fadeDuration);
-                ShowSettingsUI();
-    }
-
-    public void CloseSettingsUI()
-    {
-        if (isMenu)
-        {
-            isMenu = false;
-            menuUICG.DOFade(1, fadeDuration);
-        }
-
-        else if (isMainMenu)
-        {
-            isMainMenu = false;
-            mainMenuUICG.DOFade(1, fadeDuration);
-        }
+        StartCoroutine(ShowLoadingUI(menuUICG));
+        SetCanvasGroupInActive(charSelectionUICG);
     }
     
     public IEnumerator ShowLoadingUI(CanvasGroup targetUI)
@@ -137,7 +123,7 @@ public class UIManager : MonoBehaviour
         progressBar.value = 1f;
         progressText.text = "100%";
 
-        float briefWaitSimulation = Random.Range(0.3f, 0.8f);
+        float briefWaitSimulation = Random.Range(1f, 3);
         yield return new WaitForSeconds(briefWaitSimulation);
 
         yield return loadingUICG.DOFade(0, fadeDuration).WaitForCompletion();
@@ -152,27 +138,138 @@ public class UIManager : MonoBehaviour
             targetUI.blocksRaycasts = true;
         }
     }
-    public void OpenMainMenu()
-    {
-        SetCanvasGroupActive(mainMenuUICG);
+
+    public void StartGame()
+    {       
+        StartCoroutine(startGame());
     }
 
-    public void OpenCharacterSelectionUI()
+    private IEnumerator startGame()
     {
-        SetCanvasGroupActive(charSelectionUICG);
+        SetCanvasGroupInActive(menuUICG);
+        yield return StartCoroutine(ShowLoadingUI(charSelectionUICG));
+        yield return new WaitForSeconds(0.1f); // tiny delay buffer
+        charView.InitializeDefaultCharacter();
+        camAnim.MoveToSelectionView();
+
+    }
+
+    public void CloseCharacterSelectionUI()
+    {
+            SetCanvasGroupInActive(charSelectionUICG);
+        SetCanvasGroupActive(menuUICG);
+    }
+
+    public void loadGameSelectionUI()
+    {
+        StartCoroutine(loadOutToGameMode());
+
+        // TODO: Ragna the selected character will be registered in this method
+    }
+
+    public IEnumerator loadOutToGameMode()
+    {
+        SetCanvasGroupInActive(charSelectionUICG);
+            yield return StartCoroutine(ShowLoadingUI(gameModeUICG));
+                UIScreens[1].DOScale (Vector2.one, 0.25f).SetEase(Ease.OutBack);
+    }
+
+    public void GameModeToMenu()
+    {
+        StartCoroutine(returnToGameMode());
+    }
+
+    public IEnumerator returnToGameMode()
+    {
+        yield return UIScreens[1].DOScale (Vector2.zero, 0.20f).SetEase(Ease.OutBack);
+            SetCanvasGroupInActive(gameModeUICG);
+                StartCoroutine(ShowLoadingUI(charSelectionUICG));
+    }
+
+    public void GameModeToHosting()
+    {
+        StartCoroutine(OpenHosting());
+    }
+
+    public IEnumerator OpenHosting()
+    {
+        yield return UIScreens[1].DOScale (Vector2.zero, 0.20f).SetEase(Ease.OutBack);
+            SetCanvasGroupInActive(gameModeUICG);
+                SetCanvasGroupActive(hostingUI);
+    }
+
+    public void HostingToGameMode()
+    {
+        StartCoroutine(OpenGameMode());
+    }
+
+    public IEnumerator OpenGameMode()
+    {
+        SetCanvasGroupInActive(hostingUI);
+            SetCanvasGroupActive(gameModeUICG);
+                yield return UIScreens[1].DOScale (Vector2.one, 0.20f).SetEase(Ease.OutBack);
+    }
+
+    public void FadeInGameIDInputField()
+    {
+        SetCanvasGroupActive(gameIDUI);
+    }
+
+    public void FadeOutGameIDInputField()
+    {
+        SetCanvasGroupInActive(gameIDUI);
+    }
+
+    public void FadeInLobby()
+    {
+        SetCanvasGroupActive(lobbyUI);
+        SetCanvasGroupInActive(hostingUI);
+    }
+
+    public void FadeOutLobby()
+    {
+        SetCanvasGroupActive(hostingUI);
+        SetCanvasGroupInActive(lobbyUI);
+    }
+
+    public void PlayerReady()
+    {
+        readyText.text = "Ready...";
+
+        // TODO: Add other functionality to show player is ready, like enabling background colour
+
+        // foreach(var bg in playerImageBg)
+        // {
+        //     bg.gameObject.SetActive(false);
+        // }
+
+        // playerImageBg[selectedBgInt].SetActive(true);
+
     }
 
     #region SETTINGS UI
     private void ShowSettingsUI()
     {
-        UIScreens[5].DOScale(Vector2.one, 0.25f).SetEase(Ease.OutBack);
+        UIScreens[0].gameObject.SetActive(true);
+            UIScreens[0].DOScale(Vector2.one, 0.25f).SetEase(Ease.OutBack);
     }
-
-    public void UndoSettingsUI()
+    
+    private IEnumerator undoSettings()
     {
-        UIScreens[5].DOScale(Vector2.zero, 0.25f).SetEase(Ease.InBack);
+        yield return UIScreens[0].DOScale(Vector2.zero, 0.25f).SetEase(Ease.InBack);
+            UIScreens[0].gameObject.SetActive(false);
     }
 
+    public void CloseSettingsUI()
+    {
+        if (isMenu)
+        {
+            isMenu = false;
+                SetCanvasGroupActive(menuUICG);
+        }
+
+        StartCoroutine(undoSettings());
+    }
     #endregion
 
     private void SetCanvasGroupActive(CanvasGroup tUI)
@@ -182,6 +279,15 @@ public class UIManager : MonoBehaviour
         tUI.DOFade(1, fadeDuration);
         tUI.interactable = true;
         tUI.blocksRaycasts = true;
+    }
+
+    private void SetCanvasGroupInActive(CanvasGroup tUI)
+    {
+        tUI.gameObject.SetActive(false);
+        tUI.alpha = 0;
+        tUI.DOFade(0, fadeDuration);
+        tUI.interactable = false;
+        tUI.blocksRaycasts = false;
     }
 
     #region AUDIO
@@ -206,7 +312,6 @@ public class UIManager : MonoBehaviour
         if (sfxAS != null && buttonClick != null)
             sfxAS.PlayOneShot(buttonClick, sfxAS.volume);
     }
-
     #endregion
 
     public void Quit() => Application.Quit();  // Quit application ASAP ASAP
@@ -216,5 +321,4 @@ public class UIManager : MonoBehaviour
         Debug.Log("Load Game!");
         // This will load the gameScene using scene manager
     }
-    
 }
